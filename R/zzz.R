6f8c8b1f36e0c8b0e11cab3a48b486597fdcd7cf
TDJorgensen <TJorgensen314@gmail.com>
2024-04-25 12:42:38 +0200

    update splash

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,8 @@
     packageStartupMessage(" ")
     packageStartupMessage("###################################################################")
     packageStartupMessage("This is ", paste(pkgname, version))
-    packageStartupMessage("See the NEWS file for comparison with deprecated semTools features.")
+    packageStartupMessage("See the README file on github.com/TDJorgensen/lavaan.mi")
+    packageStartupMessage("for a table comparing it with deprecated semTools features.")
     packageStartupMessage("###################################################################")
 }
 
