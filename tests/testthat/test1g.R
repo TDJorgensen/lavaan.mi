eaf9b48071b4676b6614d46e8646bfb03a5bcb11
TDJorgensen <TJorgensen314@gmail.com>
2024-05-31 16:03:25 +0200

    update /tests

--- tests/testthat/test1g.R
+++ tests/testthat/test1g.R
@@ -1,5 +1,5 @@
 ### Terrence D. Jorgensen
-### Last updated: 9 May 2022
+### Last updated: 24 May 2024
 ### unit tests for single-group lavaan.mi object(s)
 
 library(lavaan.mi)
@@ -9,15 +9,7 @@ library(lavaan.mi)
 ## Prepare example
 ## ---------------
 
-set.seed(12345)
-HSMiss <- HolzingerSwineford1939[ , paste("x", 1:9, sep = "")]
-HSMiss$x5 <- ifelse(HSMiss$x1 <= quantile(HSMiss$x1, .3), NA, HSMiss$x5)
-HSMiss$x9 <- ifelse(is.na(HSMiss$x5), NA, HSMiss$x9)
-HSMiss$school <- HolzingerSwineford1939$school
-
-library(Amelia)
-HS.amelia <- amelia(HSMiss, m = 20, noms = "school")
-imps <- HS.amelia$imputations
+data(HS20imps) # import a list of 20 imputed data sets
 
 HS.model <- '
 visual  =~ x1 + a*x2 + b*x3
@@ -26,8 +18,8 @@ speed   =~ x7 + x8 + x9
 ab := a*b
 '
 ## fit single-group model
-fit1 <- cfa.mi(HS.model, data = imps, std.lv = TRUE, meanstructure = TRUE)
-fit0 <- cfa.mi(HS.model, data = fit1, std.lv = TRUE, meanstructure = TRUE,
+fit1 <- cfa.mi(HS.model, data = HS20imps, std.lv = TRUE, meanstructure = TRUE)
+fit0 <- cfa.mi(HS.model, data = fit1    , std.lv = TRUE, meanstructure = TRUE,
                #se = "none",
                #estimator = "mlm",
                constraints = '.p2. == .p3. ; .p5. == .p6. ; .p8. == .p9.')
@@ -42,11 +34,12 @@ test_that("objects returned by lavaan.mi are class lavaan.mi", {
   expect_s4_class(fit1, "lavaan.mi")
 })
 
-test_that("summary returns object inheriting from data.frame", {
-  sum1 <- summary(fit1, stand = TRUE, rsq = TRUE)
-  expect_true(inherits(sum1, "data.frame"))
-  expect_true(inherits(sum1, "lavaan.data.frame"))
-  expect_true(inherits(sum1, "lavaan.parameterEstimates"))
+test_that("summary returns object inheriting from lavaan.summary, with components", {
+  sum1 <- summary(fit1, stand = "std.all", rsq = TRUE, fit.measures = TRUE)
+  expect_true(inherits(sum1    , "lavaan.summary"))
+  expect_true(inherits(sum1$fit, "lavaan.vector"))
+  expect_true(inherits(sum1$pe , "data.frame"))
+  expect_true(inherits(standardizedSolution.mi(fit1), "data.frame"))
 })
 
 test_that("vectors are returned with(out) names", {
@@ -84,16 +77,19 @@ test_that("fitted and resid return lavaan matrices in $cov", {
   expect_identical(inherits(resid(fit1, type = "cor.bentler")$cov,
                             c("lavaan.matrix.symmetric","matrix"),
                             which = TRUE), expected = 1:2)
+  expect_identical(inherits(lavResiduals.mi(fit1)$cov,
+                            c("lavaan.matrix.symmetric","matrix"),
+                            which = TRUE), expected = 1:2)
 })
 
 test_that("lavTestLRT returns named vectors", {
   skip_on_cran()
-  expect_named(lavTestLRT.mi(fit1))              # pooled LRT by D4 (default)
-  expect_named(lavTestLRT.mi(fit1, test = "D3")) # pooled LRT by D3
-  expect_named(lavTestLRT.mi(fit1, test = "D2")) # pooled Wald test (D2)
-  expect_named(lavTestLRT.mi(fit1, asymptotic = TRUE)) # as chisq == F * df1
-  expect_named(lavTestLRT.mi(fit0, h1 = fit1, asymptotic = TRUE)) # compare fit
-  expect_named(lavTestLRT.mi(fit0, h1 = fit1, test = "D2"))       # using D2
+  expect_named(lavTestLRT.mi(fit1))                     # pooled LRT by D4 (default)
+  expect_named(lavTestLRT.mi(fit1, pool.method = "D3")) # pooled LRT by D3
+  expect_named(lavTestLRT.mi(fit1, pool.method = "D2")) # pooled Wald test (D2)
+  expect_named(lavTestLRT.mi(fit1, asymptotic = TRUE))  # as chisq == F * df1
+  expect_named(lavTestLRT.mi(fit0, h1 = fit1, asymptotic = TRUE))  # compare fit
+  expect_named(lavTestLRT.mi(fit0, h1 = fit1, pool.method = "D2")) # using D2
 })
 
 
@@ -109,6 +105,9 @@ test_that("score tests return lavaan data.frames", {
   expect_identical(inherits(sc0$epc, c("lavaan.data.frame","data.frame"),
                             which = TRUE), expected = 1:2)
   expect_identical(inherits(modindices.mi(fit1),
+                            c("lavaan.data.frame","data.frame"), which = TRUE),
+                   expected = 1:2)
+  expect_identical(inherits(lavTestScore.mi(fit0, pool.method = "D1")$test,
                             c("lavaan.data.frame","data.frame"), which = TRUE),
                    expected = 1:2)
 })
--- tests/testthat/test2g.R
+++ tests/testthat/test2g.R
@@ -1,5 +1,5 @@
 ### Terrence D. Jorgensen
-### Last updated: 9 May 2022
+### Last updated: 24 May 2024
 ### unit tests for multigroup lavaan.mi object(s)
 
 library(lavaan.mi)
@@ -9,15 +9,7 @@ library(lavaan.mi)
 ## Prepare example
 ## ---------------
 
-set.seed(12345)
-HSMiss <- HolzingerSwineford1939[ , paste("x", 1:9, sep = "")]
-HSMiss$x5 <- ifelse(HSMiss$x1 <= quantile(HSMiss$x1, .3), NA, HSMiss$x5)
-HSMiss$x9 <- ifelse(is.na(HSMiss$x5), NA, HSMiss$x9)
-HSMiss$school <- HolzingerSwineford1939$school
-
-library(Amelia)
-HS.amelia <- amelia(HSMiss, m = 20, noms = "school")
-imps <- HS.amelia$imputations
+data(HS20imps) # import a list of 20 imputed data sets
 
 HS.model <- '
 visual  =~ x1 + x2 + x3
@@ -25,8 +17,8 @@ textual =~ x4 + x5 + x6
 speed   =~ x7 + x8 + x9
 '
 ## fit single-group model
-fit1 <- cfa.mi(HS.model, data = imps, estimator = "mlmv", group = "school")
-fit0 <- cfa.mi(HS.model, data = imps, estimator = "mlmv", group = "school",
+fit1 <- cfa.mi(HS.model, data = HS20imps, estimator = "mlmv", group = "school")
+fit0 <- cfa.mi(HS.model, data = HS20imps, estimator = "mlmv", group = "school",
                group.equal = c("loadings","intercepts"))
 
 
@@ -39,12 +31,10 @@ test_that("objects returned by lavaan.mi are class lavaan.mi", {
   expect_s4_class(fit1, "lavaan.mi")
 })
 
-test_that("summary returns object inheriting from data.frame", {
-  sum1 <- summary(fit1, stand = "std.all",
-                  ci = FALSE, fmi = TRUE, asymptotic = TRUE, fit = TRUE)
-  expect_true(inherits(sum1, "data.frame"))
-  expect_true(inherits(sum1, "lavaan.data.frame"))
-  expect_true(inherits(sum1, "lavaan.parameterEstimates"))
+test_that("summary returns object inheriting from lavaan.summary, with components", {
+  sum1 <- summary(fit1, stand = "std.all", asymptotic = TRUE, fmi = TRUE, ci = TRUE)
+  expect_true(inherits(sum1   , "lavaan.summary"))
+  expect_true(inherits(sum1$pe, "data.frame"))
 })
 
 test_that("vectors are returned with(out) names", {
@@ -83,29 +73,36 @@ test_that("fitted and resid return lavaan matrices in $cov", {
   expect_identical(inherits(resid(fit1, type = "cor.bentler")$Pasteur$cov,
                             c("lavaan.matrix.symmetric","matrix"),
                             which = TRUE), expected = 1:2)
+  expect_identical(inherits(lavResiduals.mi(fit1)$Pasteur$cov,
+                            c("lavaan.matrix.symmetric","matrix"),
+                            which = TRUE), expected = 1:2)
 })
 
 test_that("lavTestLRT and Wald return named vectors", {
   skip_on_cran()
   waldcon <- ' .p70. == 0 ; .p71. == 0 ; .p72. == 0 '
-  expect_named(lavTestLRT.mi(fit1, asymptotic = TRUE, test = "D2"))
-  expect_named(lavTestLRT.mi(fit1, asymptotic = TRUE, test = "D2",
+  expect_named(lavTestLRT.mi(fit1, asymptotic = TRUE, pool.method = "D2"))
+  expect_named(lavTestLRT.mi(fit1, asymptotic = TRUE, pool.method = "D2",
                              pool.robust = TRUE))
-  expect_named(lavTestLRT.mi(fit0, h1 = fit1, asymptotic = TRUE, test = "D2"))
-  expect_named(lavTestLRT.mi(fit0, h1 = fit1, asymptotic = TRUE, test = "D2",
+  expect_named(lavTestLRT.mi(fit0, h1 = fit1, asymptotic = TRUE, pool.method = "D2"))
+  expect_named(lavTestLRT.mi(fit0, h1 = fit1, asymptotic = TRUE, pool.method = "D2",
                              pool.robust = TRUE))
-  expect_named(lavTestWald.mi(fit0, test = "D1", asymptotic = TRUE, constraints = waldcon))
-  expect_named(lavTestWald.mi(fit0, test = "D2", asymptotic = TRUE, constraints = waldcon))
+  expect_named(lavTestWald.mi(fit0, pool.method = "D1", asymptotic = TRUE, constraints = waldcon))
+  expect_named(lavTestWald.mi(fit0, pool.method = "D2", asymptotic = TRUE, constraints = waldcon))
 })
 
 test_that("score tests match modification indices", {
   skip_on_cran()
-  sc1 <- lavTestScore.mi(fit1, add = 'x1 ~~ x9 ; x4 ~~ x7', test = "D1",
+  suppressWarnings(
+    ## because I used a robust test statistic
+    sc1 <- lavTestScore.mi(fit1, add = 'x1 ~~ x9 ; x4 ~~ x7', pool.method = "D1",
+                           asymptotic = TRUE, epc = TRUE)
+  )
+  suppressWarnings(mi1 <- modindices.mi(fit1, op = '~~', pool.method = "D1"))
+
+  sc2 <- lavTestScore.mi(fit1, add = 'x1 ~~ x9 ; x4 ~~ x7', pool.method = "D2",
                          asymptotic = TRUE, epc = TRUE)
-  mi1 <- modindices.mi(fit1, op = '~~', test = "D1")
-  sc2 <- lavTestScore.mi(fit1, add = 'x1 ~~ x9 ; x4 ~~ x7', test = "D2",
-                         asymptotic = TRUE, epc = TRUE)
-  mi2 <- modindices.mi(fit1, op = '~~', test = "D2")
+  mi2 <- modindices.mi(fit1, op = '~~', pool.method = "D2")
 
   expect_equal(mi1$mi[(mi1$lhs == "x1" & mi1$rhs == "x9") | (mi1$lhs == "x4" & mi1$rhs == "x7")],
                sc1$uni$X2, tolerance = .0001)
